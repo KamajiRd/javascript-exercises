cf32a527c0b73da17625ba90046e6566452a7d64
KamajiRD <adamescarlos2808@gmail.com>
2022-07-30 00:05:19 -0400

    Solution exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+// const removeFromArray = function(arr, value, ...theArgs) {
 
-};
+//     return arr.filter(function(ele) {
+//         return ele != value && ele != theArgs;
+//     });
 
+// };
+
+
+const removeFromArray = function (arr, ...theArgs) {
+    return arr.filter( val => !theArgs.includes(val) )
+  };
+  
 // Do not edit below this line
 module.exports = removeFromArray;

aa05627b875c77bcc5c0ec3011b69b86a9ca8d23
KamajiRD <adamescarlos2808@gmail.com>
2022-08-03 21:19:00 -0400

    sumAll first attempt

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
 
+    let sumNumbers = [];
+    let sumResult = sumNumbers.reduce((partialSum, a) => partialSum + a, 0);
+
+    for (let i = 0; i < lastNum; i++) {
+
+        firstNum = i
+        sumNumbers.push(firstNum);
+
+    }
+
+    return sumResult;
 };
+
+
 
 // Do not edit below this line
 module.exports = sumAll;

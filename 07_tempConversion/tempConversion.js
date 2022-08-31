ff3ec3673a8ed12b58cd4813046561f305d49b19
KamajiRD <adamescarlos2808@gmail.com>
2022-08-31 18:20:30 -0400

    tempCoversion solution ctof 5th test failed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,27 @@
-const ftoc = function() {
+const ftoc = function(tempeture) {
+
+  if (!Number.isInteger(tempeture)) return "ERROR";
+
+  let operation = parseInt(tempeture) - 32;
+  let rawResult = operation * 5/9;
+  let roundedResult = Math.round(rawResult * 10) / 10;
+  
+
+  return roundedResult
 
 };
 
-const ctof = function() {
+const ctof = function(tempeture) {
+
+  // if (!Number.isInteger(tempeture)) return "ERROR";
+
+  let operation = parseInt(tempeture) * 1.8;
+  let rawResult = operation + 32 ;
+  let roundedResult = Math.round(rawResult * 10) / 10;
+  
+
+  return roundedResult
+
 
 };
 

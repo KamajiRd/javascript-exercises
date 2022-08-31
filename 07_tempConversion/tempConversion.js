de1797e16969e014bd712c722bf5d65bdd4b7699
KamajiRD <adamescarlos2808@gmail.com>
2022-08-31 18:22:36 -0400

    Used solutions branch to solve

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,30 +1,11 @@
-const ftoc = function(tempeture) {
-
-  if (!Number.isInteger(tempeture)) return "ERROR";
-
-  let operation = parseInt(tempeture) - 32;
-  let rawResult = operation * 5/9;
-  let roundedResult = Math.round(rawResult * 10) / 10;
-  
-
-  return roundedResult
-
-};
-
-const ctof = function(tempeture) {
-
-  // if (!Number.isInteger(tempeture)) return "ERROR";
-
-  let operation = parseInt(tempeture) * 1.8;
-  let rawResult = operation + 32 ;
-  let roundedResult = Math.round(rawResult * 10) / 10;
-  
-
-  return roundedResult
-
-
-};
-
+const ftoc = function(f) {
+  return Math.round((f - 32) * (5/9) * 10) / 10;
+ };
+ 
+ const ctof = function(c) {
+   return Math.round(((c * 9/5) + 32) * 10) / 10;
+ };
+ 
 // Do not edit below this line
 module.exports = {
   ftoc,

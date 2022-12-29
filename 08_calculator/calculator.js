31cb3f93a2a9d5dca676b091f751d0e051167ac6
KamajiRD <adamescarlos2808@gmail.com>
2022-12-28 21:24:43 -0400

    Solution for add and substract

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,9 +1,11 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	let sumTotal = parseInt(num1) + parseInt(num2)
+  return sumTotal
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  let substractTotal = parseInt(num1) - parseInt(num2)
+  return substractTotal
 };
 
 const sum = function() {
